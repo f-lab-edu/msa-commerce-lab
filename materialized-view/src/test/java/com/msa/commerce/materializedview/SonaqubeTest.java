45da0003a9e9541fb8fd681891cdef9f3582dfb7
joel-you <ruew12@gmail.com>
2025-08-03 12:13:25 +0900

    Add placeholder test for SonarQube analysis

---
+++ materialized-view/src/test/java/com/msa/commerce/materializedview/SonaqubeTest.java
@@ -0,0 +1,14 @@
+package com.msa.commerce.materializedview;
+
+import org.junit.Test;
+
+public class SonaqubeTest {
+
+    @Test
+    public void testSonaqube() {
+        // This is a placeholder test to ensure that the SonarQube analysis runs.
+        // You can add actual test logic here if needed.
+        System.out.println("Running SonarQube test...");
+    }
+
+}
