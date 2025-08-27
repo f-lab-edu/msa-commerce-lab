b63a0de194ff3e8dd2b48bb583d99918614b0e16
joel-you <ruew12@gmail.com>
2025-08-27 11:27:37 +0900

    feat: Implement Notification pattern for validation handling in product commands

--- monolith/src/main/java/com/msa/commerce/monolith/product/application/port/in/ProductGetUseCase.java
+++ monolith/src/main/java/com/msa/commerce/monolith/product/application/port/in/ProductGetUseCase.java
@@ -3,6 +3,7 @@ package com.msa.commerce.monolith.product.application.port.in;
 public interface ProductGetUseCase {
 
     ProductResponse getProduct(Long productId);
-    
+
     ProductResponse getProduct(Long productId, boolean increaseViewCount);
+
 }
